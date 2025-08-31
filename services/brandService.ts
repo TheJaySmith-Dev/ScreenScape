c56e4df5c5ef958d0fc8219a4bd77d1094946f2d
TheJaySmith1 <spriest-whelks.7y@icloud.com>
2025-08-31 16:47:04 +0200

    fix: Update poster aspect ratio and media for brands
    This commit adjusts the aspect ratio for brand cards to `aspect-video` to better match typical poster dimensions. It also includes updated `mediaIds` for the Hulk collection and corrects the poster URL for the Iron Man collection.

--- services/brandService.ts
+++ services/brandService.ts
@@ -8,7 +8,7 @@ const marvelCharacterCollections: Brand['characterCollections'] = [
     { 
         id: 131292, 
         name: 'Iron Man Collection', 
-        posterUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/fbeJ7f0aD4A112Bc1tnpzyn82xO.jpg', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/fbeJ7f0aD4A112Bc1tnpzyn82xO.jpg', 
         backdropUrl: 'https://image.tmdb.org/t/p/original/rI8zOWkRQJdlAyQ6WJOSlYK6JxZ.jpg',
         mediaIds: [
             { id: 1726, type: 'movie' },   // Iron Man
@@ -22,11 +22,25 @@ const marvelCharacterCollections: Brand['characterCollections'] = [
             { id: 299534, type: 'movie' }, // Avengers: Endgame
         ]
     },
-    { id: 86062, name: 'Hulk Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/25QZ5wK1y3bH22auO2iW3iA4p3.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/p3rKiC2yMW6R2O2sV1gI2btrj3x.jpg' },
+    { 
+        id: 86062, 
+        name: 'Hulk Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/25QZ5wK1y3bH22auO2iW3iA4p3.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/p3rKiC2yMW6R2O2sV1gI2btrj3x.jpg',
+        mediaIds: [
+            { id: 1724, type: 'movie' },   // The Incredible Hulk
+            { id: 24428, type: 'movie' },  // The Avengers
+            { id: 99861, type: 'movie' },  // Avengers: Age of Ultron
+            { id: 284053, type: 'movie' }, // Thor: Ragnarok
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 92783, type: 'tv' },     // She-Hulk: Attorney at Law
+        ]
+    },
     { 
         id: 131296, 
         name: 'Captain America Collection', 
-        posterUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/2tOgiY533JSFp7OrVlkeRJvsZpI.jpg', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/2tOgiY533JSFp7OrVlkeRJvsZpI.jpg', 
         backdropUrl: 'https://image.tmdb.org/t/p/original/7kVucUYqz8fZkOmFtA8opDwJFXM.jpg',
         mediaIds: [
             { id: 1771, type: 'movie' },   // Captain America: The First Avenger
@@ -39,26 +53,103 @@ const marvelCharacterCollections: Brand['characterCollections'] = [
             { id: 88396, type: 'tv' },     // The Falcon and the Winter Soldier
         ]
     },
-    { id: 131295, name: 'Thor Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/3F4HzC6y25gIFlG3y2GckD1sT3R.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/sI2lH1a02GO6z17t0qgODx1YYxW.jpg' },
-    { id: 86311, name: 'The Avengers Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/tYcmmUR1G0i2i5F4t2I1AbLhYQi.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/zuW6fOiusv4X9nnW3paqfXzFlWe.jpg' },
-    { id: 529892, name: 'Ant-Man Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/9T5WHb6IxsTRs28hG1mEW3bLd1b.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/6XN1v8mI8d2Vzdk4A6sS7n25bBF.jpg' },
-    { id: 86032, name: 'Doctor Strange Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/pMPenpL1t1VOKKECHM5sXBvplfWcwoD4l.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/7s932SSa5aY513a9p6a2jyrfLz8.jpg' },
-    { id: 529893, name: 'Spider-Man (MCU) Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/8Vl2gPk5sA02n53Tf2g2kC93s3f.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/iQFcwSGbZXMkeyKrC9MyzJbFYSx.jpg' },
-    { id: 529894, name: 'Black Panther Collection', posterUrl: 'https://image.tmdb.org/t/p/w500/yvj3vC6dGvo7E7y2a8g7s4Yx25G.jpg', backdropUrl: 'https://image.tmdb.org/t/p/original/4KrgkpHvf7vjHnEGWnPCmHjrmwP.jpg' },
+    { 
+        id: 131295, 
+        name: 'Thor Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/3F4HzC6y25gIFlG3y2GckD1sT3R.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/sI2lH1a02GO6z17t0qgODx1YYxW.jpg',
+        mediaIds: [
+            { id: 10195, type: 'movie' },  // Thor
+            { id: 24428, type: 'movie' },  // The Avengers
+            { id: 76338, type: 'movie' },  // Thor: The Dark World
+            { id: 99861, type: 'movie' },  // Avengers: Age of Ultron
+            { id: 284053, type: 'movie' }, // Thor: Ragnarok
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 616037, type: 'movie' }, // Thor: Love and Thunder
+            { id: 91363, type: 'tv' },     // What If...?
+        ]
+    },
+    { 
+        id: 86311, 
+        name: 'The Avengers Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/tYcmmUR1G0i2i5F4t2I1AbLhYQi.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/zuW6fOiusv4X9nnW3paqfXzFlWe.jpg',
+        mediaIds: [
+            { id: 24428, type: 'movie' },  // The Avengers
+            { id: 99861, type: 'movie' },  // Avengers: Age of Ultron
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+        ]
+    },
+    { 
+        id: 529892, 
+        name: 'Ant-Man Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/9T5WHb6IxsTRs28hG1mEW3bLd1b.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/6XN1v8mI8d2Vzdk4A6sS7n25bBF.jpg',
+        mediaIds: [
+            { id: 102899, type: 'movie' }, // Ant-Man
+            { id: 271110, type: 'movie' }, // Captain America: Civil War
+            { id: 363088, type: 'movie' }, // Ant-Man and the Wasp
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 640146, type: 'movie' }, // Ant-Man and the Wasp: Quantumania
+        ]
+    },
+    { 
+        id: 86032, 
+        name: 'Doctor Strange Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/v15s9Y0bsYo0i76aB5A2aN2l78i.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/7s932SSa5aY513a9p6a2jyrfLz8.jpg',
+        mediaIds: [
+            { id: 284054, type: 'movie' }, // Doctor Strange
+            { id: 284053, type: 'movie' }, // Thor: Ragnarok
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 634649, type: 'movie' }, // Spider-Man: No Way Home
+            { id: 453395, type: 'movie' }, // Doctor Strange in the Multiverse of Madness
+        ]
+    },
+    { 
+        id: 529893, 
+        name: 'Spider-Man (MCU) Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/8Vl2gPk5sA02n53Tf2g2kC93s3f.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/iQFcwSGbZXMkeyKrC9MyzJbFYSx.jpg',
+        mediaIds: [
+            { id: 271110, type: 'movie' }, // Captain America: Civil War
+            { id: 315635, type: 'movie' }, // Spider-Man: Homecoming
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 429617, type: 'movie' }, // Spider-Man: Far From Home
+            { id: 634649, type: 'movie' }, // Spider-Man: No Way Home
+        ]
+    },
+    { 
+        id: 529894, 
+        name: 'Black Panther Collection', 
+        posterUrl: 'https://image.tmdb.org/t/p/w500/yvj3vC6dGvo7E7y2a8g7s4Yx25G.jpg', 
+        backdropUrl: 'https://image.tmdb.org/t/p/original/4KrgkpHvf7vjHnEGWnPCmHjrmwP.jpg',
+        mediaIds: [
+            { id: 271110, type: 'movie' }, // Captain America: Civil War
+            { id: 284052, type: 'movie' }, // Black Panther
+            { id: 299536, type: 'movie' }, // Avengers: Infinity War
+            { id: 299534, type: 'movie' }, // Avengers: Endgame
+            { id: 505642, type: 'movie' }, // Black Panther: Wakanda Forever
+        ]
+    },
 ];
 
 export const brands: Brand[] = [
   {
     id: 'marvel',
     name: 'Marvel',
-    posterUrl: 'https://cdn.theposterdb.com/prod/public/images/posters/optimized/collections/2261/xRGEoY8xur3J56wAavmmyWqfAW0.jpg',
+    posterUrl: 'https://image.tmdb.org/t/p/w1280/avjMS5q2i32f22tJ1a6nS2pB3sC.jpg',
     companyId: MARVEL_COMPANY_ID,
     characterCollections: marvelCharacterCollections
   },
   {
     id: 'backtothefuture',
     name: 'Back to the Future',
-    posterUrl: 'https://image.tmdb.org/t/p/w500/gLLfp2AeuaSfjyIcLEMppXggE4U.jpg',
+    posterUrl: 'https://image.tmdb.org/t/p/original/5qpt23LxPU68FsQnQJslah4VF6C.jpg',
     characterCollections: [],
     mediaIds: [
         { id: 105, type: 'movie' },
