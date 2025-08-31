8c16455601b73473acc3c082764d1c91be741d01
TheJaySmith1 <spriest-whelks.7y@icloud.com>
2025-08-31 15:15:19 +0200

    fix(streamingService): Update Netflix hover GIF URL
    Corrects the `hoverGifUrl` for the Netflix streaming service to point to the correct asset.

--- services/streamingService.ts
+++ services/streamingService.ts
@@ -11,7 +11,7 @@ export const supportedProviders: StreamingProviderInfo[] = [
     name: 'Netflix',
     logoUrl: 'https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940',
     bgColor: '#000000',
-    hoverGifUrl: 'https://media.tenor.com/biAoUAAAAd/tudum-netflix.gif',
+    hoverGifUrl: 'https://media.tenor.com/biAoUAAAAC/tudum-netflix.gif',
     borderColor: '#E50914',
     edgeToEdge: true,
   },
